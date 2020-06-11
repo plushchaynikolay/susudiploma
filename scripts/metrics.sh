ee23f911ffd943bff8f3d57c3e77b381bdd48135
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-06-11 22:49:21 +0500

    123

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -1,17 +1,40 @@
+MODULE_PATH=$@
+
 # Модули
-$SHELL modules.sh
+echo "Модулей: `ls ./**/*.py | wc -l`"
+echo "Из них тестовых `ls ./**/test_*.py | wc -l`"
 
 # Классы
-$SHELL classes.sh
+echo "Классов: `grep -o 'class ' ./**/*.py | wc -l`"
+echo "Из них тестовых `grep -o 'class ' ./**/test_*.py | wc -l`"
 
 # Raw
-$SHELL LOC.sh
+echo "LOC-метрика"
+radon raw -s \
+    -i 'testing_utils,tests,kb_dialog_system,statistics,migrations' \
+    -e '*/**/__init__.py' \
+    $MODULE_PATH
 
 # Cyclomatic complexity
-$SHELL cyclomatic.complexity.sh
+echo "Cyclomatic complexity"
+radon cc -a \
+    -i 'testing_utils,tests,kb_dialog_system,statistics,migrations' \
+    -e '*/**/__init__.py' \
+    $MODULE_PATH \
+    -j -O cc.json
 
 # Halsted
-$SHELL Halsted.sh
+echo "Halsted"
+radon hal \
+    -i 'testing_utils,tests,kb_dialog_system,statistics,migrations' \
+    -e '*/**/__init__.py' \
+    $MODULE_PATH \
+    -j -O halsted.json
 
 # Maintainability
-$SHELL maintainability.sh
+echo "Maintainability"
+radon mi \
+    -i 'testing_utils,tests,kb_dialog_system,statistics,migrations' \
+    -e '*/**/__init__.py' \
+    $MODULE_PATH \
+    -j -O maintainability.json
